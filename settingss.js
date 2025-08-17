9711920e5caaf66e01b591cb3bbae94a2f531526
bboymushata-tech <bboymushata@gmail.com>
2025-08-17 11:38:30 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~gu8iCB7B#ES061hFjAcn7o9ntuIDYE-siBK7fq9WdxxQzA3DaKTw",  // add your Session Id here
+  SESSION_ID:"malvin~ZyFCBIBA#zNFm26XmXM_TXYVd3VNbqDqmqq6fvGuJJ_QrtG4OUFU",  // add your Session Id here
   
   OWNER_NUMBER: "263071 513 8544", // put your phone number here
   
